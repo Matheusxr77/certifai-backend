dcdd956c6882b86c38d81ac0086de83c1d0eb5ba
Mayara <mayeufraferreira@gmail.com>
2025-06-26 18:32:51 -0300

    Commit inicial: projeto gerado com o Spring Initializr

---
+++ src/main/java/br/com/certifai/CertifaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.certifai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/certifai/CertifaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.certifai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
