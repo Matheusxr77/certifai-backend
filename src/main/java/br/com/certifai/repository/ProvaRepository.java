6129f50ce8f1e368a6cad000a1c66287d7b38bf9
Mayara <mayeufraferreira@gmail.com>
2025-08-19 22:41:17 -0300

    [CF-0006] novos repositories

--- src/main/java/br/com/certifai/repository/ProvaRepository.java
+++ src/main/java/br/com/certifai/repository/ProvaRepository.java
@@ -3,5 +3,9 @@ package br.com.certifai.repository;
 import br.com.certifai.model.Prova;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProvaRepository extends JpaRepository<Prova, Long> {
+    List<Prova> findByCertificacaoId(Long id);
+    List<Prova> findByUsuarioId(Long id);
 }
--- src/main/java/br/com/certifai/repository/QuestaoRepository.java
+++ src/main/java/br/com/certifai/repository/QuestaoRepository.java
@@ -1,4 +1,16 @@
 package br.com.certifai.repository;
 
-public interface QuestaoRepository {
+import br.com.certifai.model.Questao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface QuestaoRepository extends JpaRepository<Questao, Long> {
+    @Query(value = "SELECT * FROM questao WHERE dificuldade = :dificuldade ORDER BY RANDOM() LIMIT :limit", nativeQuery = true)
+    List<Questao> findRandomQuestoesByDificuldade(
+            @Param("dificuldade") String dificuldade,
+            @Param("limit") int limit
+    );
 }
