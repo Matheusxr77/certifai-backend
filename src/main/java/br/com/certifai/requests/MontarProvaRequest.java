5d35dee30bc5fa527febf07b894677272744b12f
Mayara <mayeufraferreira@gmail.com>
2025-08-19 22:42:55 -0300

    [CF-0006] novo request para montar prova personalizada

--- src/main/java/br/com/certifai/requests/MontarProvaRequest.java
+++ src/main/java/br/com/certifai/requests/MontarProvaRequest.java
@@ -1,4 +1,27 @@
 package br.com.certifai.requests;
 
+import br.com.certifai.enums.Dificuldades;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class MontarProvaRequest {
+    @NotNull(message = "Usuário é obrigatório.")
+    private Long usuarioId;
+
+    @NotNull(message = "Ccertificação é obrigatório.")
+    private Long certificacaoId;
+
+    @NotNull(message = "Dificuldade é obrigatória.")
+    private Dificuldades dificuldadeQuestoes;
+
+    @Min(value = 1, message = "Número de questões deve ser no mínimo 1.")
+    private int numeroDeQuestoes;
+
+    private boolean comTempo = false;
+
+    private Long tempoEmMinutos;
 }
