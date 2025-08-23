80d1d6db2694241c7aeb806bed5bd584973999dc
Mayara <mayeufraferreira@gmail.com>
2025-08-22 21:28:10 -0300

    [CF-0007] service e interface de Alternativa

---
+++ src/main/java/br/com/certifai/service/interfaces/IAlternativaService.java
@@ -0,0 +1,15 @@
+package br.com.certifai.service.interfaces;
+
+import br.com.certifai.dto.AlternativaDTO;
+import br.com.certifai.model.Alternativa;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAlternativaService {
+    Alternativa create(Alternativa alternativa);
+    List<Alternativa> findAll();
+    Optional<Alternativa> findById(Long id);
+    Alternativa update(Long id, Alternativa alternativa);
+    void delete(Long id);
+}
